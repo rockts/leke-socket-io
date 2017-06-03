d759a3d98bd5c159fc4607356f17f1b69365999e
gaopeng <gaopeng@lekee.cc>
2017-06-03 18:24:16 +0800

    complete

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import morgan from 'morgan'
+import path from 'path'
+import socketIO from 'socket.io'
+
+
+const app = express()
+
+app.set('views', path.resolve(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(morgan('dev'))
+
+app.get('/', (request, response) => {
+  response.render('index')
+})
+
+let server = app.listen(3000, () => {
+  console.log('Listen port: 3000')
+})
+
+let io = socketIO(server)
+
+io.on('connection', (socket) => {
+  console.log('user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+  socket.on('message', (message) => {
+    console.log(message)
+    io.emit('message', message)
+  })
+})
